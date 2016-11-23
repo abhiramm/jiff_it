a083e17faebcb987ae328e9b0b14cc330c026aea
abhiramm <abhiram@railsfactory.org>
2016-11-23 18:33:09 +0530

    log addition

--- lib/jiff_it.rb
+++ lib/jiff_it.rb
@@ -2,6 +2,11 @@ require "jiff_it/version"
 
 module JiffIt
     def put_log
-      puts "Gem is working"
+    	after_create :run
+      puts "Gem is working good"
+    end
+
+    def run
+    	puts "Gem is working"
     end
 end
